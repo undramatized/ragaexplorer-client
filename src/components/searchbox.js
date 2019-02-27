20061df0b9c3b906caf139de43e48d2a328beda9
undramatized <siddarth21@hotmail.com>
2019-02-27 23:57:03 +0800

    fixed search by filtering function

--- src/components/searchbox.js
+++ src/components/searchbox.js
@@ -1,8 +1,8 @@
 import React from "react"
 import {AutoComplete} from "antd"
 
-const Option = AutoComplete.Option;
-const OptGroup = AutoComplete.OptGroup;
+// const Option = AutoComplete.Option;
+// const OptGroup = AutoComplete.OptGroup;
 
 const ragalist = [
   "Shanmugapriya",
@@ -12,11 +12,37 @@ const ragalist = [
 ]
 
 class SearchBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: []
+    };
+  }
+
+  onSelect = (value) => {
+    console.log('onSelect', value);
+  }
+
+  handleSearch = (inputVal) => {
+    let results = []
+    if (inputVal.length>0) {
+      results = ragalist.filter((value)=>{
+        return inputVal.toLowerCase() == value.toLowerCase().slice(0,inputVal.length)
+      })
+    }
+
+   this.setState({ dataSource: results })
+  }
+
   render () {
     return (
       <div>
         <AutoComplete
-          dataSource = {ragalist}
+          dataSource={this.state.dataSource}
+          style={{ width: 200 }}
+          onSelect={this.onSelect}
+          onSearch = {this.handleSearch}
+          placeholder="Search for Raaga here..."
         />
       </div>
     )
